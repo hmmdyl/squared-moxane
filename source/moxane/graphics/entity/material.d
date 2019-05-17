4dd559f0ec0511c9b237a45fba65bb973c57faa5
Dylan Graham <dylan.graham2000@outlook.com>
2019-05-17 13:19:35 +1000

    Discarding moxane.graphics.entity

---
+++ source/moxane/graphics/entity/material.d
@@ -0,0 +1,13 @@
+module moxane.graphics.entity.material;
+
+import moxane.core.engine;
+import moxane.core.asset;
+
+import moxane.graphics.effect;
+
+class Material
+{
+	Moxane moxane;
+
+	
+}
---
+++ source/moxane/graphics/standard.d
@@ -0,0 +1,18 @@
+module moxane.graphics.standard;
+
+import moxane.graphics.effect;
+
+class Material
+{
+	Effect effect;
+}
+
+class Model
+{
+
+}
+
+class StandardRenderer
+{
+
+}
