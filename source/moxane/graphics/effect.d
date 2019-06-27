456970ff6fae08d4290c92810a5a7987ca68cd3a
Dylan Graham <dylan.graham2000@outlook.com>
2019-06-27 23:40:57 +1000

    New effect params

--- source/moxane/graphics/effect.d
+++ source/moxane/graphics/effect.d
@@ -28,21 +28,23 @@ struct EffectUniform
 	public void set(float v) { glUniform1f(location, v); }
 	public void set(double v) { glUniform1d(location, v); }
 
+	public void set(float* v, int num) { glUniform1fv(location, num, v); }
+
 	public void set(Vector2f v) { glUniform2f(location, v.x, v.y); }
-	public void set(Vector2f* v) { glUniform2fv(location, 1, v.arrayof.ptr); }
+	public void set(Vector2f* v, int num = 1) { glUniform2fv(location, num, v.arrayof.ptr); }
 	public void set(Vector2d v) { glUniform2d(location, v.x, v.y); }
-	public void set(Vector2d* v) { glUniform2dv(location, 1, v.arrayof.ptr); }
+	public void set(Vector2d* v, int num = 1) { glUniform2dv(location, num, v.arrayof.ptr); }
 	void set(Vector2i v) { glUniform2i(location, v.x, v.y); }
 
 	public void set(Vector3f v) { glUniform3f(location, v.x, v.y, v.z); }
-	public void set(Vector3f* v) { glUniform3fv(location, 1, v.arrayof.ptr); }
+	public void set(Vector3f* v, int num = 1) { glUniform3fv(location, num, v.arrayof.ptr); }
 	public void set(Vector3d v) { glUniform3d(location, v.x, v.y, v.z); }
-	public void set(Vector3d* v) { glUniform3dv(location, 1, v.arrayof.ptr); }
+	public void set(Vector3d* v, int num = 1) { glUniform3dv(location, num, v.arrayof.ptr); }
 
 	public void set(Vector4f v) { glUniform4f(location, v.x, v.y, v.z, v.w); }
-	public void set(Vector4f* v) { glUniform4fv(location, 1, v.arrayof.ptr); }
+	public void set(Vector4f* v, int num = 1) { glUniform4fv(location, num, v.arrayof.ptr); }
 	public void set(Vector4d v) { glUniform4d(location, v.x, v.y, v.z, v.w); }
-	public void set(Vector4d* v) { glUniform4dv(location, 1, v.arrayof.ptr); }
+	public void set(Vector4d* v, int num = 1) { glUniform4dv(location, num, v.arrayof.ptr); }
 
 	public void set(Matrix4f* m, bool transpose = false) { glUniformMatrix4fv(location, 1, transpose, m.arrayof.ptr); }
 }
