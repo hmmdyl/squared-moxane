38506bf4375da4c789352909b986dee1addf37b1
Dylan Graham <dylan.graham2000@outlook.com>
2019-07-11 17:58:17 +1000

    Completed maybe

--- source/moxane/utils/maybe.d
+++ source/moxane/utils/maybe.d
@@ -1,20 +1,92 @@
 module moxane.utils.maybe;
 
 import std.exception : enforce;
+import std.traits;
+import core.memory;
 
 struct Maybe(T)
+	if(is(T == struct) || isBasicType!T)
 {
 	private T payload;
-	private bool isNull;
+	private bool notNull;
+
+	this(T payload, bool isNull = false)
+	{
+		this.payload = payload;
+		this.notNull = !isNull;
+	}
+
+	@property bool isNull() const { return !notNull; }
+
+	void opAssign(T item)
+	{
+		payload = item;
+		notNull = true;
+	}
 
 	T* unwrap()
 	{
-		enforce(!isNull, "Payload is null.");
+		enforce(notNull, "Payload is null.");
 		return &payload;
 	}
 
-	T* unwrapOr(T nulled = T.init)
+	T* unwrapOr()
 	{
-
+		if(!notNull) payload = T.init;
+		return &payload;
 	}
+
+	T* unwrapOrElse(T def)
+	{
+		if(!notNull) payload = def;
+		return &payload;
+	}
+
+	T* unwrapOrElse(T* def)
+	{
+		if(notNull) return &payload;
+		else return def;
+	}
+
+	bool opEquals(auto ref const Maybe!T other) const
+	{ return notNull == other.notNull && payload == other.payload; }
+}
+
+struct MaybePtr(T)
+	if(is(PointerTarget!T* == struct) || isBasicType!(PointerTarget!T*))
+{
+	private T* ptr;
+	private bool notNull;
+	private bool safeAlloced;
+
+	this(T* ptr)
+	{
+		this.ptr = ptr;
+		notNull = ptr !is null;
+	}
+
+	~this()
+	{
+		if(safeAlloced)
+		{
+			GC.free(ptr);
+			ptr = null;
+			safeAlloced = false;
+		}
+	}
+
+	T* get() { return ptr; }
+
+	T* getSafe()
+	{
+		if(!notNull)
+		{
+			ptr = new T;
+			safeAlloced = true;
+		}
+		return ptr;
+	}
+
+	bool opEquals(auto ref const MaybePtr!T other) const
+	{ return ptr is other.ptr; }
 }
